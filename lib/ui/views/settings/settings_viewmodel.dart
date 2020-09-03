720d7f2c5cb83220f5384a80c577face948b1a9d
Vasyl Prokopyshyn <vasiachess@gmail.com>
2020-09-03 17:03:08 +0300

    Add Clear survey data dialog

--- lib/ui/views/settings/settings_viewmodel.dart
+++ lib/ui/views/settings/settings_viewmodel.dart
@@ -7,6 +7,7 @@ class SettingsViewModel extends BaseViewModel {
 
   final NavigationService _navigationService = locator<NavigationService>();
   final SnackbarService _snackbarService = locator<SnackbarService>();
+  final _dialogService = locator<DialogService>();
 
 
   Future navigateLanguage() async {
@@ -17,7 +18,20 @@ class SettingsViewModel extends BaseViewModel {
     await _navigationService.navigateTo(Routes.settingsSaveModeView);
   }
 
-  Future clearData() {
+  Future clearData() async {
+
+    DialogResponse dialogResponse = await _dialogService.showConfirmationDialog(
+      title: 'Clear survey data?',
+      description: 'Are you sure that you want to clear all unsynchronized survey data saved in this device?\nThis action can not be undone.',
+      barrierDismissible: true,
+      dialogPlatform: DialogPlatform.Cupertino,
+      confirmationTitle: 'Clear Data',
+      cancelTitle: 'Cancel'
+    );
+
+    if (dialogResponse.confirmed) {
+      // make some action
+    }
 
   }
 
